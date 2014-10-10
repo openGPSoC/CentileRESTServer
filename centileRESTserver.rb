6f15388e70529ed38ff4788948b0728bd2ca3490
pacharanero <marcusbaw@gmail.com>
2014-10-10 10:05:29 +0100

    cleanup

--- centileRESTserver.rb
+++ centileRESTserver.rb
@@ -1,8 +1,6 @@
 require 'sinatra'
 require 'json'
 
-lms_hash = JSON.parse(File.read('data/lmsdata.json'))
-
 get '/height/:height' do
   centile = Centile.new
   "Height: #{centile.height}"
@@ -14,26 +12,4 @@ end
 
 get 'bmicent/:bmi' do
   "BMI: #{params[:bmi]}"
-end
-
-class Centile
-    def height
-        return 56
-    end
-    
-    def weight
-        
-    end
-    
-    def height_and_weight
-        
-    end
-    
-    def bmi
-        
-    end
-    
-    def bmi_centile
-        
-    end
 end
